f2b0e8fff8be834a88c49c6f18fa5dd672d166c6
Sean Keane <seankeane93@gmail.com>
2025-02-19 19:22:35 -0800

    wip developer page talking with backend/sql, working on updating state for certification changes.

--- portfolio-page-backend/obj/Debug/net8.0/portfolio-page-backend.AssemblyInfo.cs
+++ portfolio-page-backend/obj/Debug/net8.0/portfolio-page-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio-page-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263693106c9bfdbfa398a9aef95a3f5d4b8d075e")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio-page-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio-page-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
